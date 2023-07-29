39211cc2c81ffba8ebc0009a0989b15c960d3993
Kashish Lakhara <kashishlakhara04@gmail.com>
2023-07-29 15:24:07 +0530

    typescript updated

---
+++ BirdSquawk-Service/src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from 'mongoose';
+import { router } from "./routes/routes";
+
+const app = express();
+app.use(express.json());
+app.use(router);
+
+const Startup =async () => {
+    try{
+        await mongoose.connect('mongodb://birdsquawk-mongo-service:27017/birdsquawk');
+    } catch (e) {
+        console.log(e);
+    }
+
+app.listen(5000, () => {
+    console.log('Birdsquaek-Service is listening on port 5000');
+});
+};
+
+Startup();

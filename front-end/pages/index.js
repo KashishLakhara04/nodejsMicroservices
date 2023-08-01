223e320f6cee91e6889ae82d0e03907554327c79
Kashish Lakhara <kashishlakhara04@gmail.com>
2023-08-01 10:43:48 +0530

    skaffold configured

--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -5,20 +5,33 @@ import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+function Home({ data }) {
   return (
-    <>
+    <div className={styles.container}>
       <Head>
         <title>Bird Squawk</title>
         <meta name="description" content="Microservices app" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Bird Squawk
+          Welcome to Bird Squawk - Name: {data.name}, Age: {data.age}
         </h1>
       </main>
-    </>
+    </div>
   )
 }
+
+// This gets called on every request
+export async function getServerSideProps(){
+  //Fetch data from external API
+  const res = await fetch(`https://api.agify.io?name=Birdy`)
+  const data = await res.json()
+  console.log(data);
+
+  // Pass data to the page via props
+  return { props: { data } }
+}
+
+export default Home;
